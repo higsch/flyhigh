a04f752b9c7a43ce4326c44bb67b28545f289eb3
Matthias Stahl <matthias.stahl87@web.de>
2019-01-24 00:59:05 +0100

    initial commit

---
+++ flyhigh.py
@@ -0,0 +1,40 @@
+from time import sleep
+from random import randint
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+def buildQueryURL(fr, to, date):
+  url = 'https://www.google.com/flights#flt={fr}.{to}.{date};c:SEK;e:1;s:0;sd:1;t:f;tt:o'
+  url = url.format(fr = fr, to = to, date = date)
+  return url
+
+def getDriver(url):
+  driver = webdriver.Chrome('chromedriver')
+  sleep(4)
+  driver.get(url)
+  sleep(randint(2, 3))
+  return driver
+
+def parseFlights(driver):
+  flights = driver.find_elements_by_class_name('gws-flights-results__result-item')
+  return [parseLiItem(li) for li in flights]
+
+def parseLiItem(li):
+  return {
+    'time': li.find_element_by_class_name('gws-flights-results__times').text,
+    'price': li.find_element_by_class_name('gws-flights-results__price').text,
+    'flight_number': li.get_attribute('data-fp'),
+    'seating': li.find_element_by_class_name('gws-flights-results__seating-class-be-non-be').text,
+    'meta': li.find_element_by_class_name('gws-flights-results__leg-flight').text
+  }
+
+def main():
+  url = buildQueryURL('ARN', 'FRA', '2019-02-14')
+  print(url)
+  driver = getDriver(url)
+  flights = parseFlights(driver)
+  print(flights)
+  driver.quit()
+
+if __name__ == '__main__':
+	main()
