88210403aed49fe632bcd45b8110e5a75dcdc7c4
Matthias Stahl <matthias.stahl87@web.de>
2019-02-25 22:57:43 +0100

    parse flights

--- flyhigh.py
+++ flyhigh.py
@@ -1,10 +1,10 @@
 from time import sleep
-from random import randint
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
-def buildQueryURL(fr, to, date):
-  url = 'https://www.google.com/flights#flt={fr}.{to}.{date};c:SEK;e:1;s:0;sd:1;t:f;tt:o'
-  url = url.format(fr = fr, to = to, date = date)
+def buildQueryURL(fr, to, date, currency):
+  url = 'https://www.google.com/flights#flt={fr}.{to}.{date};c:{currency};e:1;s:0;sd:1;t:f;tt:o'
+  url = url.format(fr = fr, to = to, date = date, currency = currency)
   return url
 
 def getDriver(url):
@@ -12,27 +12,27 @@ def getDriver(url):
   options.add_argument('headless')
   driver = webdriver.Chrome(chrome_options = options)
   driver.get(url)
+  sleep(2)
   return driver
 
 def parseFlights(driver):
-  flights = driver.find_elements_by_class_name('gws-flights-results__result-item')
-  return [parseLiItem(li) for li in flights]
+  def parsePrice(price):
+    return int(''.join([i for i in price.split() if i.isdigit()]))
 
-def parseLiItem(li):
-  return {
-    'time': li.find_element_by_class_name('gws-flights-results__times').text,
-    'price': li.find_element_by_class_name('gws-flights-results__price').text,
-    'flight_number': li.get_attribute('data-fp'),
-    'seating': li.find_element_by_class_name('gws-flights-results__seating-class-be-non-be').text,
-    'meta': li.find_element_by_class_name('gws-flights-results__leg-flight').text
-  }
+  flightsSoup = BeautifulSoup(driver.page_source, 'lxml')
+  for flight in flightsSoup.find_all('li', class_ = 'gws-flights-results__result-item'):
+    yield {
+      'id': flight['data-fp'],
+      'times': flight.find('div', class_ = 'gws-flights-results__times').text,
+      'price': parsePrice(flight.find('div', class_ = 'gws-flights-results__price').text)
+    }
 
 def main():
-  url = buildQueryURL('ARN', 'FRA', '2019-02-14')
+  url = buildQueryURL('ARN', 'FRA', '2019-03-14', 'SEK')
   print(url)
   driver = getDriver(url)
   flights = parseFlights(driver)
-  print(flights)
+  print([flight for flight in flights])
   driver.quit()
 
 if __name__ == '__main__':
