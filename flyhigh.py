bbd49e76cad8b02d9a6571f6f5b33eb580a74789
Matthias Stahl <matthias.stahl87@web.de>
2019-02-25 23:49:55 +0100

    write flights to db

--- flyhigh.py
+++ flyhigh.py
@@ -3,6 +3,8 @@ import datetime
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+import sqlite3
+
 def buildQueryURL(fr, to, date, currency):
   url = 'https://www.google.com/flights#flt={fr}.{to}.{date};c:{currency};e:1;s:0;sd:1;t:f;tt:o'
   url = url.format(fr = fr, to = to, date = date, currency = currency)
@@ -27,19 +29,53 @@ def parseFlights(driver, date):
   for flight in flightsSoup.find_all('li', class_ = 'gws-flights-results__result-item'):
     times = parseTimes(flight.find('div', class_ = 'gws-flights-results__times').text, date)
     yield {
-      'id': flight['data-fp'],
+      'flightId': flight['data-fp'],
       'departure': times[0],
       'arrival': times[1],
-      'price': parsePrice(flight.find('div', class_ = 'gws-flights-results__price').text)
+      'duration': flight.find('div', class_ = 'gws-flights-results__duration').text,
+      'price': parsePrice(flight.find('div', class_ = 'gws-flights-results__price').text),
+      'timestamp': datetime.datetime.now()
     }
 
+def connectDB(dbName):
+  db = sqlite3.connect(dbName)
+  cur = db.cursor()
+  cur.execute('''
+    CREATE TABLE IF NOT EXISTS flights(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      flightId TEXT,
+      departure DATETIME,
+      arrival DATETIME,
+      duration TEXT,
+      price INTEGER,
+      timestamp DATETIME)
+    ''')
+  db.commit()
+  return db
+
+def writeFlights(db, flights):
+  cur = db.cursor()
+  for flight in flights:
+    print('Writing to db... ', flight)
+    cur.execute('''
+      INSERT INTO flights(flightId, departure, arrival, duration, price, timestamp) 
+        VALUES(:flightId, :departure, :arrival, :duration, :price, :timestamp)
+      ''', flight)
+  db.commit()
+
 def main():
+  # get flights
   date = '2019-03-14'
   url = buildQueryURL('ARN', 'FRA', date, 'SEK')
   print(url)
   driver = getDriver(url)
   flights = parseFlights(driver, date)
-  print([flight for flight in flights])
+
+  # write flights to database
+  db = connectDB('flights.sqlite')
+  with db:
+    writeFlights(db, flights)
+
   driver.quit()
 
 if __name__ == '__main__':
