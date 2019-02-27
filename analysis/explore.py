ee636cc407c4af82bfc527537a6a14b5949aa9e2
Matthias Stahl <matthias.stahl87@web.de>
2019-02-27 23:49:11 +0100

    added first exploratory analysis

---
+++ analysis/explore.py
@@ -0,0 +1,63 @@
+#%% [markdown]
+# # Explore flight data
+
+#%%
+import os
+from pathlib import Path
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+cwd = Path().resolve()
+dbFileName = os.path.join(cwd, 'db', 'flights.sqlite')
+conn = sqlite3.connect(dbFileName)
+
+#%% [markdown]
+# ## fetch a fixed flight over departure time
+
+#%%
+def reduceLabels(labels):
+  i = 0
+  for label in labels:
+    if (i % 20 == 0):
+      label.set_visible(True)
+    else:
+      label.set_visible(False)
+    i += 1
+#%%
+df = pd.read_sql_query('''
+  SELECT *, strftime('%Y-%m-%d %H', timestamp) AS bookingTime
+  FROM flights
+  WHERE flightId = 'ARNFRA0LH809'
+  ORDER BY flightDate
+''', conn)
+fig, ax = plt.subplots()
+ax.xaxis_date()
+for key, group in df.groupby('bookingTime'):
+  ax.plot(group['flightDate'], group['price'], label = key)
+reduceLabels(ax.xaxis.get_ticklabels())
+ax.legend(loc = 'center left', bbox_to_anchor = (1, 0.5))
+fig.autofmt_xdate()
+plt.show()
+
+#%% [markdown]
+# ## fetch all flights over departure time
+
+#%%
+df = pd.read_sql_query('''
+  SELECT *, strftime('%Y-%m-%d %H', timestamp) AS bookingTime
+  FROM flights
+  WHERE bookingTime = '2019-02-26 23'
+  ORDER BY flightDate
+''', conn)
+fig, ax = plt.subplots()
+ax.xaxis_date()
+for key, group in df.groupby('flightId'):
+  ax.plot(group['flightDate'], group['price'], label = key)
+reduceLabels(ax.xaxis.get_ticklabels())
+ax.legend(loc = 'center left', bbox_to_anchor = (1, 0.5))
+fig.autofmt_xdate()
+plt.show()
+
+#%%
+conn.close()
