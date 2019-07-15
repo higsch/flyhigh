fa099ba0908b2a50beb46883128e9d6291d760e2
higsch <matthias.stahl87@web.de>
2019-07-15 23:23:38 +0200

    better correlation analysis, also timestamp based

--- analysis/flyhighR/main.R
+++ analysis/flyhighR/main.R
@@ -36,7 +36,7 @@ flights$flightId <-  gsub(pattern = "ARNFRA0", replacement = "", x = flights$fli
 # round timestamp and departure to halfdays
 roundHalfDay <- function (daytime, switchTz = FALSE) {
   if (switchTz) {
-    dt <- daytime %>% with_tz("Europe/Stockholm")
+    dt <- daytime + dhours(2) #%>% with_tz("Europe/Stockholm")
   } else {
     dt <- daytime
   }
@@ -90,12 +90,12 @@ flights.forCorr <- flights %>%
 
 # mutate
 flights.forCorr <- flights.forCorr %>%
-  mutate(timeToDepartureDays = as.numeric(timeToDepartureRounded)/(24*60*60)) %>%
+  mutate(timeToDepartureDays = floor(2*as.numeric(timeToDepartureRounded)/(24*60*60))/2) %>%
   select(flightIdUnique, timeToDepartureDays, price)
 
 # check for duplicates
 allFlights <- unique(flights.forCorr$flightIdUnique)
-any(sapply(X = allFlights,
+sum(sapply(X = allFlights,
            FUN = function (flight) {
              flights.forCorr %>%
                filter(flightIdUnique == flight) %>%
@@ -118,8 +118,11 @@ flights.wide <- flights.forCorr %>%
 
 # calculate the correlation matrix
 cor.mat <- cor(flights.wide,
-               method = "kendall",
+               method = "spearman",
                use = "pairwise.complete.obs")
+
+# replace NAs
+cor.mat[is.na(cor.mat)] <- 0
 
 # hierarchical clustering
 hCluster <- hclust(d = dist(cor.mat,
@@ -153,3 +156,86 @@ flights.forCorr %>%
   ggplot(aes(x = timeToDepartureDays, y = price, group = flightIdUnique, color = flightIdUnique)) +
   geom_line() +
   scale_x_reverse()
+
+# "standard" flight
+flights.forCorr %>%
+  filter(flightIdUnique %in% c("LH801_2019_4_2_10_0", "LH801_2019_4_3_10_0", "SK635_2019_4_5_9_0", "LH809_2019_4_3_6_5",
+                               "SK635_2019_4_4_9_0", "SK637_2019_4_4_14_35", "SK673_2019_4_4_17_0")) %>%
+  ggplot(aes(x = timeToDepartureDays, y = price, group = flightIdUnique, color = flightIdUnique)) +
+  geom_line() +
+  scale_x_reverse()
+
+# midsummer flights
+flights.forCorr %>%
+  filter(grepl("2019_6_2[0-4]", flightIdUnique)) %>%
+  ggplot(aes(x = timeToDepartureDays, y = price, group = flightIdUnique, color = flightIdUnique)) +
+  geom_line() +
+  geom_point() +
+  scale_x_reverse()
+
+
+#############
+# correlation analysis with timestamp
+#############
+
+# only one timepoint per day and only up to today
+flights.forCorr <- flights %>%
+  filter(departureRounded < today()) %>%
+  filter(!grepl("LH622[79]", flightId))
+
+# mutate
+flights.forCorr <- flights.forCorr %>%
+  select(flightIdUnique, timestampRounded, price)
+
+# check for duplicates
+allFlights <- unique(flights.forCorr$flightIdUnique)
+sum(sapply(X = allFlights,
+           FUN = function (flight) {
+             flights.forCorr %>%
+               filter(flightIdUnique == flight) %>%
+               select(timestampRounded) %>%
+               unlist() %>%
+               duplicated() %>%
+               any()
+           }))
+
+# plot
+flights.forCorr %>%
+  ggplot(aes(x = timeToDepartureDays, y = price, group = flightIdUnique)) +
+  geom_line() +
+  scale_x_reverse()
+
+# make a wide table
+flights.wide <- flights.forCorr %>%
+  spread(key = flightIdUnique, value = price) %>%
+  column_to_rownames("timeToDepartureDays")
+
+# calculate the correlation matrix
+cor.mat <- cor(flights.wide,
+               method = "spearman",
+               use = "pairwise.complete.obs")
+
+# replace NAs
+cor.mat[is.na(cor.mat)] <- 0
+
+# hierarchical clustering
+hCluster <- hclust(d = dist(cor.mat,
+                            method = "euclidean"),
+                   method = "average")
+cor.mat <- cor.mat[hCluster$order, hCluster$order] %>%
+  as.data.frame() %>%
+  rownames_to_column("flightIdUnique")
+
+# merge with meta data
+cor.mat <- flights %>%
+  filter(!duplicated(flightIdUnique)) %>%
+  right_join(cor.mat, by = "flightIdUnique")
+
+# export it
+write.table(x = cor.mat,
+            file = file.path("output", "price_correlation_matrix.txt"),
+            quote = FALSE,
+            sep = "\t",
+            row.names = TRUE,
+            col.names = NA)
+
