82e7a5aa6a3fa827d6e3f33685d8854db129c3e5
higsch <matthias.stahl87@web.de>
2019-07-19 12:12:24 +0200

    started building svelte app

---
+++ visualisation/app/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<style>
+  /* Delete all preset margins */
+  :global(*) {
+    margin: 0;
+    padding: 0;
+  }
+
+  /* Set the global variables */
+  :global(:root){
+    --low-price: #EF6461;
+    --end-price: #E3D8F1;
+    --high-price: #EF6461;
+  }
+</style>
